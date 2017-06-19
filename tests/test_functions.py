00bdfdc16da09d4d4922d2a5d5d518420c612d81
Ashley Ong <ashley_2461994@hotmail.com>
2017-06-19 15:32:11 +0800

    fix encode test

--- tests/test_functions.py
+++ tests/test_functions.py
@@ -3,25 +3,14 @@ from PyBeacon import PyBeacon
 def test_encode():
 	urlToEncode = "https://goggles.com"
 	data = [
-		0x03,  
-		0x03,  
-		0xAA, 0xFE, 
-		0x0E,  
-		0x16,  
-		0xAA, 0xFE,
-		0x10,  
-		0xF8, 
-		0x03, 
-		'g',
-		'o',
-		'g',
-		'g',
-		'l',
-		'e',
-		's',
+		3,  
+		ord('g'),
+		ord('o'),
+		ord('g'),
+		ord('g'),
+		ord('l'),
+		ord('e'),
+		ord('s'),
 		0x07, 
 	]
 	assert PyBeacon.encodeurl(urlToEncode) == data
-
-def test_success():
-	assert 1 == 1
