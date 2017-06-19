7017e1a91c9fadeba2d28e2f8d80c17f27de4245
Ashley Ong <ashley_2461994@hotmail.com>
2017-06-19 14:13:18 +0800

    fix imports

--- PyBeacon/test_functions.py
+++ PyBeacon/test_functions.py
@@ -1,29 +1,24 @@
-from PyBeacon import encodeurl
+from PyBeacon import PyBeacon
 
 def test_encode():
-	urlToEncode = "https://goo.gl/a0mnsS"
+	urlToEncode = "https://goggles.com"
 	data = [
-	  0x03,  #Length of Service List
-	  0x03,  #Param: Service List
-	  0xAA, 0xFE,  # Eddystone ID
-	  0x13,  # Length of Service Data
-	  0x16,  # Service Data
-	  0xAA, 0xFE,# Eddystone ID
-	  0x10,  #Frame type: URL
-	  0xF8, # Power
-	  0x03, # https://
-	  'g',
-	  'o',
-	  'o',
-	  '.',
-	  'g',
-	  'l',
-	  '/',
-	  'a',
-	  '0',
-	  'm',
-	  'n',
-	  's',
-	  'S',
+		0x03,  
+		0x03,  
+		0xAA, 0xFE, 
+		0x0E,  
+		0x16,  
+		0xAA, 0xFE,
+		0x10,  
+		0xF8, 
+		0x03, 
+		'g',
+		'o',
+		'g',
+		'g',
+		'l',
+		'e',
+		's',
+		0x07, 
 	]
-	assert encodeurl(urlToEncode) == data
+	assert PyBeacon.encodeurl(urlToEncode) == data
